427cf4ddc9f872830c1c5670924e4d5757538b5a
jnguyen0690 <46301698+jnguyen0690@users.noreply.github.com>
2019-09-28 13:46:05 -0700

    Update joining A variables missing value as 0.R
    Moved A8 to be after a7

--- VisGroupProject/joining A variables missing value as 0.R
+++ VisGroupProject/joining A variables missing value as 0.R
@@ -2,3 +2,4 @@ data<- read.csv(file.choose(),header=T)
 total.data <- data[grep("Total, all enterprises", data$Size.of.enterprise),]
 total.data[is.na(total.data)] <- 0
 total.data$A8.Total.Incident <- total.data$A1..Disrupt.or.Deface + total.data$A2..Steal.Information + total.data$A3..Steal.Money + total.data$A4..Steal.Intellectual.Property + total.data$A5..Access.Unauthorized.Area + total.data$A6..Monitor.and.Track.Activity + total.data$A7..Unknown.Motive
+total.data <- total.data[,c(1:9,55,10:54)]
